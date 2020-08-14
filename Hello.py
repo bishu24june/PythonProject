5e450a7fb51321023c13711e49a3b1b4cf3ac529
Bishwa Ranjan Sarkar <bishu24june@gmail.com>
2020-08-14 15:14:56 +0530

    First py program

---
+++ Hello.py
@@ -0,0 +1,7 @@
+#print('This is my first Python code')
+#print (2 + 3)
+
+number1 = float( input("Enter The First Number : "))
+number2 = float( input("Enter The Second Number : "))
+
+print("Sum of  number is : = ",round((number1+number2),2))
